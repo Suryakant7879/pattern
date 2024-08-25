978f08ea2f745520962ce6f72d8965071bdc6343
Suryakant Sahu <suryakantsahu7879@gmail.com>
2024-08-26 00:18:08 +0530

    10th pattern added

---
+++ 10th.py
@@ -0,0 +1,26 @@
+
+# * * * * *
+#  * * * *
+#   * * *
+#    * *
+#     *
+#     *
+#    * *
+#   * * *
+#  * * * *
+# * * * * *
+
+
+num=int(input("enter number"))
+for i in range(num,0,-1):
+    print(" "*(num-i),end="")
+    for j in range(1,i+1,1):
+        print("*",end=" ")
+    print()    
+
+for i in range(1,num+1,1):
+    print(" "*(num-i),end="")
+    for i in range(1,i+1,1):
+        print("*",end=" ")
+    print()    
+    
